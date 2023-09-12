2cbe8353b67b39fe8b244ec04b93c656b256e010
LasseThomsenSkole <lasse@Lasses-Air.lan>
2023-09-12 09:43:04 +0200

    Changed loop to do while and added switch

--- src/Main.java
+++ src/Main.java
@@ -6,26 +6,30 @@ public class Main {
     public static void main(String[] args) {
         //menu input
         Scanner keyboard = new Scanner(System.in);
+        int menuInput;
 
-        System.out.println("1. opret 9. afslut");
-        int menuInput = keyboard.nextInt();
+
         //programloop
-        while (menuInput != 9) {
-            if(menuInput == 1) {
 
-                Superheroliste database = new Superheroliste();
+        do {
+            System.out.println("1. opret 9. afslut");
+            menuInput = keyboard.nextInt();
+            switch (menuInput) {
+                case 1:
+                    Superheroliste database = new Superheroliste();
 
-                System.out.println("indtast kaldenavn, rigtige navn, superkræft, årstal, om det er et menneske og styrketal");
+                    System.out.println("indtast kaldenavn, rigtige navn, superkræft, årstal, om det er et menneske og styrketal");
 
-                database.tilføjSuperhero(keyboard.next(), keyboard.next(), keyboard.next(), keyboard.nextInt(), keyboard.nextBoolean(), keyboard.nextDouble());
-                System.out.println(database.toString());
+                    database.tilføjSuperhero(keyboard.next(), keyboard.next(), keyboard.next(), keyboard.nextInt(), keyboard.nextBoolean(), keyboard.nextDouble());
+                    System.out.println(database.toString());
+                    break;
+
+                case 2:
+                    System.exit(0);
             }
 
-            {
-                break;
-            }
+        } while (menuInput != 9);
 
-        }
 
     }
 }
--- src/Superheroliste.java
+++ src/Superheroliste.java
@@ -1,16 +1,19 @@
 import java.util.Arrays;
 
 public class Superheroliste {
-    int count = 0;
+
+    //fejl med count
+    int count = 1;
     Superhero[] Superheroliste;
     public Superheroliste() {
         this.Superheroliste = new Superhero[5];
     }
     public void tilføjSuperhero(String name, String realName, String superPower, int yearCreated, boolean isHuman, double strength){
         Superheroliste[count++] = new Superhero(name, realName, superPower, yearCreated, isHuman, strength);
+        count += 1;
     }
 
-    @Override
+
     public String toString() {
         return "Superheroliste{" +
                 "nr." + count +
